97b1f59b9831aa75a0610bf98971afc788faa7e0
liapisn <nikos@liapis.info>
2023-08-06 21:18:57 +0300

    chore: Auth module

--- src/common/types/auth.ts
+++ src/common/types/auth.ts
@@ -1,4 +1,16 @@
+import "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      credentials: Credentials;
+    }
+  }
+}
+
 export type Credentials = {
   username: string;
   password: string;
 };
+
+export {};
