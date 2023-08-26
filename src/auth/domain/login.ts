1dae5cabeaf0c858ad2acb871b9ad4e21dec7a0a
liapisn <nikos@liapis.info>
2023-08-26 13:32:53 +0300

    fix: Error handling

--- src/auth/domain/login.ts
+++ src/auth/domain/login.ts
@@ -1,17 +1,11 @@
 import { login } from "../gateway/login";
-import { LoginError } from "../../common/errors/LoginError";
-import { APIError } from "../../common/errors/RestApiError";
-import { HttpStatusCode } from "../../common/types/http.model";
 
 class LoginCommandHandler {
   handle = async (command: LoginCommand) => {
-    try {
-      await login(command.username, command.password);
-    } catch (e) {
-      if (e instanceof LoginError)
-        throw new APIError(HttpStatusCode.UNAUTHORIZED, "Wrong credentials");
-      throw e;
-    }
+    await login(command.username, command.password);
+
+    //TODO ENCRYPT CREDENTIALS
+    return JSON.stringify(command);
   };
 }
 
