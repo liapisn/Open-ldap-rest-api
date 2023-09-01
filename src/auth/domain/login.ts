b3d7bd6a5a1c6962abfe648a182de12897b563f5
liapisn <nikos@liapis.info>
2023-09-01 19:50:09 +0300

    feature: Cookie encryption decryption

--- src/auth/domain/login.ts
+++ src/auth/domain/login.ts
@@ -1,11 +1,21 @@
+import CryptoJS from "crypto-js";
 import { login } from "../gateway/login";
+
+const ENCRYPTION_KEY =
+  process.env.ENCRYPTION_KEY || "e0IcJnxB5kbEA2qYI4U3Gfd+zdRmoabc";
 
 class LoginCommandHandler {
   handle = async (command: LoginCommand) => {
     await login(command.username, command.password);
 
-    //TODO ENCRYPT CREDENTIALS
-    return JSON.stringify(command);
+    return this.encryptCredentials(command);
+  };
+
+  private encryptCredentials = (command: LoginCommand): string => {
+    return CryptoJS.AES.encrypt(
+      JSON.stringify(command),
+      ENCRYPTION_KEY
+    ).toString();
   };
 }
 
